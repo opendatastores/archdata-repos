2f141238ac403681a4b88ad66f256c184401bbe1
Panit Tuangsuwan <tpanitte@hotmail.com>
2020-06-16 08:24:39 +0700

    v3.0.0-alpha.97

--- packages/archdatastores/src/IStoresConnect.ts
+++ packages/archdatastores/src/IStoresConnect.ts
@@ -2,6 +2,6 @@ import { IDataContext } from "archdatacore";
 
 export interface IStoresConnect<DataContext extends IDataContext = IDataContext> {
   connect: (options?: boolean | any) => Promise<DataContext>;
-  use: (handler: (context: DataContext) => Promise<void>) => IStoresConnect;
+  use: (handler: (context: DataContext) => Promise<void>) => IStoresConnect<DataContext>;
   [key: string]: any;
 }
