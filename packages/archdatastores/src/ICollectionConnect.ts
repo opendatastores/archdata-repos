72d5bc8e0354fded7615392a745cd7802247cbba
Panit Tuangsuwan <tpanitte@hotmail.com>
2020-06-16 07:17:27 +0700

    v3.0.0-alpha.96

--- packages/archdatastores/src/ICollectionConnect.ts
+++ packages/archdatastores/src/ICollectionConnect.ts
@@ -1,4 +1,5 @@
 export interface ICollectionConnect<Collection = any> {
   connect: () => Promise<Collection>;
-  use: (Func: (collection: Collection) => Promise<void>) => ICollectionConnect<Collection>;
+  use: (handler: (collection: Collection) => Promise<void>) => ICollectionConnect<Collection>;
+  [key: string]: any;
 }
--- packages/archdatastores/src/index.ts
+++ packages/archdatastores/src/index.ts
@@ -9,6 +9,8 @@ export {
 
 export { DataConnects } from "./DataConnects";
 export { DataStores } from "./DataStores";
+export { ICollectionConnect } from "./ICollectionConnect";
+export { IStoresConnect } from "./IStoresConnect";
 export { IDataConfig } from "./IDataConfig";
 export { IDataConfigs } from "./IDataConfigs";
 export { IDataConnects } from "./IDataConnects";
