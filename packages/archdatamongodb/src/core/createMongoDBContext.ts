eddb3ac164edf02703ec1457898fbb7cf8ea615f
Panit Tuangsuwan <tpanitte@hotmail.com>
2020-06-26 15:09:53 +0700

    v3.0.0-alpha.99

--- packages/archdatamongodb/src/core/createMongoDBContext.ts
+++ packages/archdatamongodb/src/core/createMongoDBContext.ts
@@ -28,6 +28,7 @@ export const createMongoDBContext = (Config: IMongoDBConnectorConfig, Options: I
 
   const ConnectCollection = async (collection: string) => {
     const DBInstance = await ConnectDB();
+    await DBInstance.createCollection(collection);
 
     return DBInstance.collection(collection);
   };
